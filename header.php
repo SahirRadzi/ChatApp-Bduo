ee9d4f4502eed78be6b26e85862f6333d6ed426e
SahirRadzi <sahir.radzi@gmail.com>
2023-05-07 12:41:56 +0800

    Install ChatApp-Bduo

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal & Edited By Sahir Radzi  -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PM SINI | ChatApp B'duo</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
